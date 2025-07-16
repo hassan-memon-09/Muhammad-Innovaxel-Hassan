9d2eceeb9ef846bb98a980ba92f441f9c72f0d21
hassan-memon-09 <muhammadhassan.pitp@gmail.com>
2025-07-16 21:57:17 +0500

    schema

---
+++ Backend/models/url.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  shortCode: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  accessCount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model('Url', urlSchema);

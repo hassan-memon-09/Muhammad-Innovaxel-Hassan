95aeeb4fde72619e6b041fd8b2277cf7ebd68cad
hassan-memon-09 <muhammadhassan.pitp@gmail.com>
2025-07-16 21:59:38 +0500

    routes

---
+++ Backend/routes/urlRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const urlController = require('../controllers/urlController');
+
+router.post('/shorten', urlController.createShortUrl);
+router.get('/shorten/:shortCode', urlController.getOriginalUrl);
+router.put('/shorten/:shortCode', urlController.updateShortUrl);
+router.delete('/shorten/:shortCode', urlController.deleteShortUrl);
+router.get('/shorten/:shortCode/stats', urlController.getUrlStats);
+router.get('/:shortCode', urlController.redirectToOriginalUrl);
+
+module.exports = router;
